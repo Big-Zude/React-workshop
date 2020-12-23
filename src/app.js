813e4a4632db550e47f7d82d9f21d4d4cf7d4c97
Zude Mwango <zudemwango@Zudes-MBP.local.tld>
2020-12-23 17:25:36 +0200

    added reach router to our project for navigation

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
-import 'regenerator-runtime/runtime'
+import {Link, Router} from "@reach/router";
+import 'regenerator-runtime/runtime';
+import Details from "./Details";
 
 const App = () => {
   return (
     <React.StrictMode>
     <div>
-      <h1 id="something-important">Adopt Me</h1>
-      <SearchParams />
+      <header>
+        <Link to="/">
+          Adopt Me!
+        </Link>
+      </header>
+      <Router>
+      <SearchParams path="/"/>
+      <Details path="/details/:id"/>
+      </Router>
     </div>
     </React.StrictMode>
   );
